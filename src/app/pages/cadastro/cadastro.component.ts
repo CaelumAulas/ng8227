61b37a43d02bc4ebd2a866e524fa516b50139ed2
Vanessa Me Tonini <vanessametonini@gmail.com>
2019-01-31 11:32:51 -0200

    Template driven validation e httpClient post

--- src/app/pages/cadastro/cadastro.component.ts
+++ src/app/pages/cadastro/cadastro.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { NgForm } from '@angular/forms';
+import { Foto } from 'src/app/models/foto';
 
 @Component({
   selector: 'cp-cadastro',
@@ -7,18 +10,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CadastroComponent implements OnInit {
 
-  foto = {
-    url: '',
-    titulo: 'valor velho',
-    descricao: ''
-  }
+  foto = new Foto();
+  mensagem = '';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
 
   ngOnInit() {}
 
-  salvar(){
-    console.log(this.foto)
+  salvar(formCadastro: NgForm){
+
+    this.http
+        .post(
+          'http://localhost:3000/v1/fotos',
+          this.foto
+        )
+        .subscribe(
+          () => {
+            this.mensagem = `Foto ${this.foto.titulo} cadastrada com sucesso!!`;
+            formCadastro.reset()
+            this.foto = new Foto();
+
+            setTimeout(() => {
+              this.mensagem = '';
+            }, 3000)
+
+          }
+          , erro => console.log(erro)
+        )
+
   }
 
 }
